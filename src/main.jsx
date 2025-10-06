7086452073cac5a8ee3f3aa711d6452a40e32ceb
SooTriss <tristansooaru@ut.ee>
2025-10-06 22:18:42 +0300

    Landing page and react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // Tailwind CSS
+import LandingPage from "./drone_landing_page.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
